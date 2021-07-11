f9e18d44611795dc99f7ea68690f9a730cedac18
Ali Haider <justalihaider@gmail.com>
2021-07-11 06:30:10 +0300

    Site Updatd

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+// document.querySelector('.message').textContent = 'Ali';
+
+document.querySelector('.check').addEventListener('click', function () {
+
+  let guess = Number(document.querySelector('.guess-number').value);
+  console.log(guess, typeof guess);
+
+  //When there is no value
+  if (!guess) {
+    document.querySelector('.message').textContent = 'You must add number to guess';
+  }
+
+})
