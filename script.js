460180b2e37775a47686882db7b4092a179899e0
Ali Haider <justalihaider@gmail.com>
2021-07-11 07:30:30 +0300

    Game Logic Implemented

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 'use strict'
 
 // document.querySelector('.message').textContent = 'Ali';
+// Generate Random Number
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 5;
+
+document.querySelector('.secret-number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function () {
 
@@ -10,6 +17,28 @@ document.querySelector('.check').addEventListener('click', function () {
   //When there is no value
   if (!guess) {
     document.querySelector('.message').textContent = 'You must add number to guess';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Congratulation! You GUESS the Right Number';
+    document.querySelector('.highscore').textContent = score * 5;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'You should guess lower Number';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over, Good luck for Next Time';
+      document.querySelector('.score').textContent = 0;
+    }
+
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Guess Heigher Number';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over, Good luck for Next Time';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 
 })
