1bf51d577c1bbed05d191005b68683fadba4d14f
Ali Haider <justalihaider@gmail.com>
2021-07-13 07:01:50 +0300

    New UI of Guess Game

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 'use strict'
-
-
 // document.querySelector('.message').textContent = 'Ali';
 // Generate Random Number
 
@@ -9,19 +7,18 @@ let score = 5;
 
 // document.querySelector('.secret-number').textContent = secretNumber;
 
-document.querySelector('.retry').setAttribute('disabled', "disabled");
+document.querySelector('.retry').classList.add('is-disabled')
 
 document.querySelector('.check').addEventListener('click', function () {
 
   let guess = Number(document.querySelector('.guess-number').value);
   console.log(guess, typeof guess);
 
-
   //When there is no value
   if (!guess) {
     document.querySelector('.message').textContent = 'You must add number to guess';
-
     document.querySelector('.message').style.color = 'red';
+
   } else if (guess === secretNumber) {
     document.querySelector('.secret-number').textContent = secretNumber;
     document.querySelector('.message').textContent = '🎉 Congratulation! You GUESS the Right Number';
@@ -30,11 +27,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.message').style.color = 'red';
     document.querySelector('.secret-number').style.backgroundColor = '#e74c3c';
-    document.querySelector('.retry').classList.remove('btn-secondary');
-    document.querySelector('.retry').classList.add('btn-warning');
-    // document.querySelector('.check').classList.remove('btn-danger');
-    document.querySelector('.check').setAttribute('disabled', "disabled");
-    document.querySelector('.retry').removeAttribute('disabled', 'disabled')
+
+    document.querySelector('.retry').classList.remove('is-disabled');
+    document.querySelector('.check').classList.add('is-disabled');
 
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -48,8 +43,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
 
       document.querySelector('.message').style.color = 'red';
-      document.querySelector('.retry').removeAttribute('disabled', 'disabled');
-      document.querySelector('.check').setAttribute('disabled', 'disabled');
+      document.querySelector('.retry').classList.remove('is-disabled');
+      document.querySelector('.check').classList.add('is-disabled');
     }
 
   } else if (guess < secretNumber) {
@@ -62,27 +57,26 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Game Over, Good luck for Next Time';
       document.querySelector('.message').style.color = 'red';
       document.querySelector('.score').textContent = 0;
-
-      document.querySelector('.retry').removeAttribute('disabled', 'disabled');
-      document.querySelector('.check').setAttribute('disabled', 'disabled');
+      document.querySelector('.retry').classList.remove('is-disabled');
+      document.querySelector('.check').classList.add('is-disabled');
     }
   }
 
 })
 
+
 //Reset Game Data
 document.querySelector('.retry').addEventListener('click', function () {
-  document.querySelector('.retry').classList.remove('btn-warning');
-  document.querySelector('.retry').classList.add('btn-secondary');
   score = 5;
+  document.querySelector('.guess-number').value = '';
   document.querySelector('.score').textContent = score;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector('.secret-number').textContent = secretNumber;
   document.querySelector('.message').textContent = 'Start Gussing the Number';
   document.querySelector('.secret-number').style.backgroundColor = '#000000';
   document.querySelector('.message').style.color = '#000000';
-  document.querySelector('.retry').setAttribute('disabled', 'disabled');
-  document.querySelector('.check').removeAttribute('disabled', 'disabled');
-  document.querySelector('body').style.backgroundColor = '#FBBB02';
+  document.querySelector('.retry').classList.add('is-disabled');
+  document.querySelector('.check').classList.remove('is-disabled');
+  document.querySelector('body').style.backgroundColor = 'white';
   document.querySelector('.secret-number').textContent = '?';
 });
